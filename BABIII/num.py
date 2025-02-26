90c6d68a3d35819bd6c644c0c26a2b1b1032b43d
Mouse <akbar.aditya.forum@gmail.com>
2025-02-26 08:07:12 +0800

    sunshine

---
+++ BABIII/num.py
@@ -0,0 +1,31 @@
+# Dalam file ini kita mempelajari mengenai angka:
+
+# operator adalah '+', '-', '*', '/', '%', tanda kurung '()' dapat digunakan untuk grouping, dan-
+# '**' adalah pangkat
+
+# contoh:
+2+2 # output adalah 4
+
+50 - 5*6 # output adalah 35
+
+(50-5*6) / 4 # output adalah 5.0
+8 / 5 # output adalah 1.6
+# catatan: hasil pembagian akan selalu berupa bilangan desimal
+
+17 / 3 # output adalah 5.666666666666667, pembagian tradisional berupa bilangan desimal/float
+17 // 3 # output adalah 5, berarti hasil pembagian adalah bilangan bulat
+17 % 3 # output adalah 2, berarti sisa pembagian adalah 2
+
+5 * 3 + 2 # output adalah 17
+5 + 3 * 2 # output adalah 11
+
+5**2 # output adalah 25, 5 pangkat 2
+2**5 # output adalah 32, 2 pangkat 5
+
+# nilai dimasukkan pada variable
+lebar = 20
+tinggi = 5*9
+luas = lebar * tinggi
+luas
+
+# untuk operan '_' hanya berlaku pada terminal interaktif
